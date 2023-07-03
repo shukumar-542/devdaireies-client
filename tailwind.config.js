7f5d9c3bf313c5be75560d64c380ca90d3ec9251
Yeasir Arafat <ysarafat.bd@gmail.com>
2023-07-03 22:42:57 +0600

    add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        title: ['Inter', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
