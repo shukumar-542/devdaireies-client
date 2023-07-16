b81a9aa3a694111b261f3fc6206894e916971ebb
Towhidul Islam <towhidulislam2.bd@gmail.com>
2023-07-16 14:01:01 +0600

    slider addded

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import Header from './shared/Header/Header'
 import Footer from './shared/Footer/Footer'
 import Team from '@/components/Team'
 import PopularPosts from '@/components/PopularPosts'
+import HomeSlider from '@/components/HomeSlider/HomeSlider'
 
 export default function Home() {
   
   return (
     <main className=" w-[90%] mx-auto">
       <Header></Header>
+      <HomeSlider></HomeSlider>
       <PopularPosts />
       <Team />
       <Footer></Footer>
