1c10d76cc24a34532b239af30353b5d5a45e229e
shukumar-542 <shukumar542@gmail.com>
2023-07-18 20:32:10 +0600

    added search

--- src/app/allBlogs/page.js
+++ src/app/allBlogs/page.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import { FaThumbsUp } from "react-icons/fa";
 const page = () => {
     const [allBlogs, setAllBlogs] = useState([]);
+    const [searchText,setSearchText] = useState('')
 
     useEffect(() => {
         const fetchRecentBlogs = async () => {
@@ -19,11 +20,30 @@ const page = () => {
         fetchRecentBlogs();
     }, []);
 
+    const handleSearch = () => {
+        fetch(`http://localhost:5000/blog/${searchText}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setAllBlogs(data);
+            });
+    }
+
+
     return (
         <>
             {/* Search */}
             <div>
 
+                <div className="flex flex-col  justify-center items-center">
+                    <div className="form-control py-10">
+                        <div className="input-group">
+                            <input type="text" placeholder="Search…" onChange={(e) => setSearchText(e.target.value)} className="input input-bordered" />
+                            <button className="btn btn-square" onClick={handleSearch} >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
             {/* Blogs */}
             <div className='grid grid-cols-3 gap-6'>
