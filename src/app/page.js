b0c429fbc6cc280c54e73b410d53ce736f7e520f
Sudipto Das <shiningsudipto@gmail.com>
2023-07-18 14:07:04 +0600

    recent post fixed

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="my-container">
       <RecentSlider />
       <PopularPosts />
-      {/* <Timeline /> */}
+      <Timeline />
       <Team />
       <Footer></Footer>
 
