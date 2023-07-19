5b149e1b0cfc522377cd49cb1973e4cae4266899
shukumar-542 <shukumar542@gmail.com>
2023-07-19 14:21:54 +0600

    added search option

--- src/app/allBlogs/page.js
+++ src/app/allBlogs/page.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-
+import Zoom from 'react-reveal/Zoom';
 import { FaThumbsUp } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 const page = () => {
     const [allBlogs, setAllBlogs] = useState([]);
     const [searchText, setSearchText] = useState("");
-    // const [blogs, setBlogs] = useState([])
+    const [result, setResult] = useState([])
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    console.log(dropdownVisible);
+
     useEffect(() => {
         const fetchRecentBlogs = async () => {
             try {
@@ -24,55 +28,83 @@ const page = () => {
 
     // handle search blog
     const handleSearch = () => {
+        setDropdownVisible(false);
         fetch(`http://localhost:5000/blog/${searchText}`)
             .then((res) => res.json())
             .then((data) => {
                 setAllBlogs(data);
             });
     }
-    // console.log(allBlogs);
+    const fetchData = (value) => {
+        fetch('http://localhost:5000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                const result = data.filter((blogs) => {
+                    return blogs && blogs.subcategory.toLowerCase().includes(value)
+                })
+                setDropdownVisible(value !== '');
+                setResult(result);
+            })
+    }
+
+    const handleChange = (value) => {
+        setSearchText(value)
+        fetchData(value)
+    }
+
+
 
     return (
         <>
             {/* Search */}
-            <div className="flex flex-col  justify-center items-center">
-            <div className="form-control py-10">
-                <div className="input-group">
-                    <input type="text" placeholder="Search…" onChange={(e) => setSearchText(e.target.value)} className="input input-bordered" />
-                    <button className="btn btn-square" onClick={handleSearch} >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                    </button>
+            <div className="flex flex-col pt-5 justify-center sticky top-14 z-20 items-center ">
+                <div className=" w-1/2 mx-auto">
+                    <div className=" border rounded-xl overflow-hidden flex items-center bg-white gap-1">
+                        <FiSearch className='ps-2 text-3xl'></FiSearch>
+                        <input type="text" className="w-full focus:outline-none p-2" placeholder="Type To Search…" onChange={(e) => handleChange(e.target.value)}  />
+                       
+                    </div>
                 </div>
-            </div>
+                <div className='bg-white  shadow-lg rounded-lg w-1/2 absolute z-10 mt-44  mx-auto max-h-28 dropdown-content overflow-y-auto'>
+                    {
+                        dropdownVisible && result.map((res, _id) => {
+                            return <div className='text-center cursor-pointer hover:bg-gray-100 py-2' key={_id} onClick={() => handleSearch(res.subcategory)}>{res.subcategory}</div>
+                        })
+                    }
+                </div>
             </div>
             {/* Blogs */}
             {
-                allBlogs.length > 0 ? <div className='grid grid-cols-3 gap-6 my-container'>
-                {
-                    allBlogs.map((blog, index) => (
-                        <div className="" key={index}>
-                            <div className="p-3 rounded-md hover:shadow-xl shadow-purple">
-                                <div className="">
-                                    <Image src={blog.image} alt='' className='rounded-md h-[220px] mb-2' height={300} width={400} />
-                                </div>
-                                <div className="space-y-3">
-                                    <div className='flex justify-between mt-3'>
-                                        <p className='border border-purple rounded-md py-1 px-3 hover:bg-purple hover:text-white transition ease-in-out duration-300 text-lightOrange font-semibold'>{blog.category}</p>
-                                        <p className='border border-purple rounded-md py-1 px-3 hover:bg-purple hover:text-white transition ease-in-out duration-300 text-lightOrange font-semibold'>{blog.subcategory}</p>
+                allBlogs.length > 0 ? <div className='grid grid-cols-3 gap-6 relative z-0 my-container'>
+                    {
+                        allBlogs.map((blog, index) => (
+
+                            <div className="" key={index}>
+                                <Zoom>
+                                    <div className="p-3 rounded-md hover:shadow-xl shadow-purple">
+                                        <div className="">
+                                            <Image src={blog.image} alt='' className='rounded-md h-[220px] mb-2' height={300} width={400} />
+                                        </div>
+                                        <div className="space-y-3">
+                                            <div className='flex justify-between mt-3'>
+                                                <p className='border  rounded-md py-1 px-3 hover:bg-orange-600 hover:text-white transition ease-in-out duration-300 text-lightOrange font-semibold'>{blog.category}</p>
+                                                <p className='border  rounded-md py-1 px-3 hover:bg-orange-600 hover:text-white transition ease-in-out duration-300 text-lightOrange font-semibold'>{blog.subcategory}</p>
+                                            </div>
+                                            <h2 className="font-bold  text-2xl">{blog.title}</h2>
+                                            <div className='flex justify-between items-center'>
+                                                <p className='flex items-center text-lg cursor-pointer font-semibold  transition-transform transform-gpu hover:-translate-y-1'><FaThumbsUp className='text-deepOrange me-1' /> {blog.likes}</p>
+                                                <button class="px-3 py-1 bg-gradient-to-r from-purple to-deepOrange text-white font-semibold rounded-md transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
+                                                    Read More
+                                                </button>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <h2 className="font-bold text-purple text-2xl">{blog.title}</h2>
-                                    <div className='flex justify-between items-center'>
-                                        <p className='flex items-center text-lg cursor-pointer font-semibold  transition-transform transform-gpu hover:-translate-y-1'><FaThumbsUp className='text-deepOrange me-1' /> {blog.likes}</p>
-                                        <button class="px-3 py-1 bg-gradient-to-r from-purple to-deepOrange text-white font-semibold rounded-md transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg">
-                                            Read More
-                                        </button>
-                                    </div>
-                                </div>
+                                </Zoom>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>  : <div className='text-2xl flex justify-center font-bold'>No Blog Available</div>
+
+                        ))
+                    }
+                </div> : <div className='text-2xl flex justify-center font-bold'>No Blog Available</div>
             }
         </>
     );
