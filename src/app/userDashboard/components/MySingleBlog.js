634b0bcc30c591978b10cadac885d56aa11f207a
Sudipto Das <shiningsudipto@gmail.com>
2023-07-22 22:50:15 +0600

    my blog done

--- src/app/userDashboard/components/MySingleBlog.js
+++ src/app/userDashboard/components/MySingleBlog.js
@@ -1,21 +1,26 @@
 import Image from 'next/image';
 import { FcCalendar, FcManager, FcRight } from "react-icons/fc";
 import img1 from '../../../../public/DD-f-logo.png';
-const MySingleBlog = () => {
+import Link from 'next/link';
+const MySingleBlog = ({ singleBlog }) => {
+    const { title, image, _id, description, author, date } = singleBlog;
     return (
         <>
             <div className='flex relative items-center p-5 bg-white rounded-md hover:shadow-lg'>
                 <div className='me-8 '>
-                    <Image src={img1} className='relative' alt='' height={150} width={300} />
+                    <Image src={image} className='relative' alt='' height={150} width={300} />
                 </div>
                 <p className='absolute top-0 right-0 py-4 px-8 text-3xl bg-purple text-white font-bold'>1</p>
                 <div>
-                    <h2 className='text-2xl font-bold my-4'>Lorem ipsum dolor sit amet consectetur.</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quam eveniet unde officia. Aliquam laboriosam suscipit mollitia, maxime minus doloremque?</p>
+                    <h2 className='text-2xl font-bold my-4'>{title}</h2>
+                    <p>{description.slice(0, 50)}...</p>
                     <div className='flex justify-between mt-3'>
-                        <p className='flex items-center text-lg'><FcManager className='text-3xl' /> By Author Name</p>
-                        <p className='flex items-center text-lg'><FcCalendar className='text-3xl' /> Date</p>
-                        <p className='flex items-center text-lg'><FcRight className='text-3xl' /> Read More</p>
+                        <p className='flex items-center text-lg'><FcManager className='text-3xl' /> By {author} </p>
+                        <p className='flex items-center text-lg'><FcCalendar className='text-3xl' /> {date} </p>
+                        {/* TODO-> Read more need to fix */}
+                        <Link href={`allBlogs/${_id}`}>
+                            <p className='flex items-center text-lg'><FcRight className='text-3xl' /> Read More</p>
+                        </Link>
                     </div>
                 </div>
             </div>
