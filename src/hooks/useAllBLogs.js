83fc674f1a3bb3351f1b83bba2430cb22d80ef8d
Sudipto Das <shiningsudipto@gmail.com>
2023-07-23 23:11:08 +0600

    refetch added

--- src/hooks/useAllBLogs.js
+++ src/hooks/useAllBLogs.js
@@ -1,6 +1,6 @@
 // 'useBlogs'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 const useBlogs = () => {
@@ -8,22 +8,28 @@ const useBlogs = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchAllBlogs = async () => {
-            try {
-                const response = await axios.get('http://localhost:5000/blogs');
-                const fetchedData = response.data;
-                setAllBlogs(fetchedData);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-                setLoading(false);
-            }
-        };
-        fetchAllBlogs();
+    const fetchAllBlogs = useCallback(async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/blogs');
+            const fetchedData = response.data;
+            setAllBlogs(fetchedData);
+            setLoading(false);
+        } catch (error) {
+            setError(error);
+            setLoading(false);
+        }
     }, []);
 
-    return { allBlogs, loading, error };
+    useEffect(() => {
+        fetchAllBlogs();
+    }, [fetchAllBlogs]);
+
+    const refetchBlogs = () => {
+        setLoading(true);
+        fetchAllBlogs();
+    };
+
+    return { allBlogs, loading, error, refetchBlogs };
 };
 
 export default useBlogs;
